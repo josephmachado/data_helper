ef022fa6093f4bdc8b58c7ba568bbbe18e3ef301
JosephKevinMachado <josephkevinmachado@pop-os.fios-router.home>
2024-05-22 14:08:13 -0400

    2024-05-22-14-08-13

--- index/index_manager.py
+++ index/index_manager.py
@@ -3,11 +3,18 @@ from llama_index.core import (SimpleDirectoryReader, StorageContext,
 
 
 def create_index(data_folder: str = "data"):
+    """
+    Function to read the input from the ./data folder
+    and create vector indexes from it
+    """
     documents = SimpleDirectoryReader(data_folder).load_data()
     index = VectorStoreIndex.from_documents(documents)
     index.storage_context.persist()
 
 
 def get_index(persist_dir: str = "./storage"):
+    """
+    Function to read the index stored at the ./storage folder location
+    """
     storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
     return load_index_from_storage(storage_context)
